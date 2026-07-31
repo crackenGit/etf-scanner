9f324b11fb659e4a3758fb7d91ed10149aff1a00
crackenGit <mr.tobias.roeder@gmail.com>
2026-07-31 13:38:40 +0200

    test portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -16,9 +16,9 @@ PORTFOLIO = [
         "partially_sold": True,  # True setzen, sobald Tranche 1 verkauft wurde
         "t1_sell_date": "2026-07-30",     # Datum von Teilverkauf 1 (z. B. '2026-07-15')
         "t1_sell_price": 24.42,    # Tatsächlicher Verkaufskurs von T1 (Optional: Falls leer, wird der EMA50 genutzt)
-        "sold": True,  # True setzen, sobald Tranche 2 verkauft wurde
-        "t2_sell_date": "2026-07-31",     # Datum von Teilverkauf 2 (z. B. '2026-07-15')
-        "t2_sell_price": 24.82,    # Tatsächlicher Verkaufskurs von T2 
+        "sold": False,  # True setzen, sobald Tranche 2 verkauft wurde
+        "t2_sell_date": None,     # Datum von Teilverkauf 2 (z. B. '2026-07-15')
+        "t2_sell_price": None,    # Tatsächlicher Verkaufskurs von T2 
 
     },
     {
